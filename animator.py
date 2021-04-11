c46b75c5202477065b2552a68b238aaea7bb0eef
Julian Tallar <jtallar@itba.edu.ar>
2021-04-11 19:40:35 -0300

    Base animation video

--- animator.py
+++ animator.py
@@ -3,26 +3,42 @@ GREEN = ' 0 255 0'
 RED = ' 255 0 0'
 C = '1e-10 '
 
+def write_corners(ovito_file, N, L):
+    ovito_file.write(str(N+4))
+    corners = '\n\n'+C+'0 0 0'+WHITE+'\n'+C+'0 '+str(L)+' 0'+WHITE+'\n'+C+str(L)+' 0 0'+WHITE+'\n'+C+str(L)+' '+str(L)+' 0'+WHITE+'\n'
+    ovito_file.write(corners)
+
 dynamic_file = open("dynamic.txt", "r")
-dynamic_file.readline()
 
 static_file = open("static.txt", "r")
 N = int(static_file.readline())
 L = float(static_file.readline())
+particle_radius = []
+for line in static_file:
+    particle_radius.append(line.split()[0])
 
 ovito_file = open("simu.xyz", "w")
-ovito_file.write(str(N+4))
-corners = '\n\n'+C+'0 0 0'+WHITE+'\n'+C+'0 '+str(L)+' 0'+WHITE+'\n'+C+str(L)+' 0 0'+WHITE+'\n'+C+str(L)+' '+str(L)+' 0'+WHITE
-ovito_file.write(corners)
 
 # TODO: Mostramos el vector velocidad en la animacion?
-for i in range(N):
-    line = dynamic_file.readline().split(' ')
-    (x,y,r) = (line[0]+' ', line[1]+' ', static_file.readline().split()[0]+' ')
-    p_id = str(i)+' '
+restart = True
+p_id = 0
+for linenum, line in enumerate(dynamic_file):
+    if restart:
+        time = float(line.rstrip())
+        write_corners(ovito_file, N, L)
+        restart = False
+        p_id = 0
+        continue
+    if "*" == line.rstrip():
+        restart = True
+        continue
+
+    line_vec = line.rstrip().split(' ')
+    (x,y,r) = (line_vec[0]+' ', line_vec[1]+' ', particle_radius[p_id]+' ')
     color = WHITE
-    ovi_line = '\n'+r+x+y+p_id+color
+    ovi_line = r+x+y+str(p_id)+' '+color+'\n'
     ovito_file.write(ovi_line)
+    p_id += 1
 
 print(f'Generated simu.xyz')
 
