e0d418cc3afe0ede67555749a2882322d00cc3ea
Julian Tallar <jtallar@itba.edu.ar>
2021-04-11 15:45:19 -0300

    Particle methods

--- src/main/java/ar/edu/itba/sds/objects/Particle.java
+++ src/main/java/ar/edu/itba/sds/objects/Particle.java
@@ -2,12 +2,13 @@ package ar.edu.itba.sds.objects;
 
 public class Particle {
     private final int id;
-    private final double x;
-    private final double y;
-    private final double vx;
-    private final double vy;
+    private double x;
+    private double y;
+    private double vx;
+    private double vy;
     private final double r;
     private final double m;
+    private int collisionCount;
 
     /**
      * Create a particle with radius
@@ -27,6 +28,7 @@ public class Particle {
         this.vy = vy;
         this.r = r;
         this.m = m;
+        this.collisionCount = 0;
     }
 
     public double centerDistance(Particle other) {
@@ -35,6 +37,93 @@ public class Particle {
 
     public double borderDistance(Particle other) {
         return centerDistance(other) - r - other.r;
+    }
+
+
+    /**
+     * @return duration of time until particle collides with a vertical wall
+     *         returns null if vx = 0
+     */
+    public Double collidesX(double leftWallX, double rightWallX) {
+        if (vx > 0) return (rightWallX - r - x) / vx;
+        if (vx < 0) return (leftWallX + r - x) / vx;
+        return null;
+    }
+
+    /**
+     * @return duration of time until particle collides with a horizontal wall
+     *         returns null if vy = 0
+     */
+    public Double collidesY(double bottomWallY, double topWallY) {
+        if (vy > 0) return (topWallY - r - y) / vy;
+        if (vy < 0) return (bottomWallY + r - y) / vy;
+        return null;
+    }
+
+    /**
+     * @param other particle to collide with
+     * @return duration of time until particle collides with other
+     *         returns null if they never collide
+     */
+    public Double collides(Particle other) {
+        // Xi = self, Xj = other
+        double deltaX = other.x - this.x, deltaY = other.y - this.y;
+        double deltaVX = other.vx - this.vx, deltaVY = other.vy - this.vy;
+        double sigma = this.r + other.r;
+
+        double prodDeltaR = deltaX * deltaX + deltaY * deltaY;
+        double prodDeltaV = deltaVX * deltaVX + deltaVY * deltaVY;
+        double prodCross = deltaVX * deltaX + deltaVY * deltaY;
+
+        if (prodCross >= 0) return null;
+
+        double d = prodCross * prodCross - prodDeltaV * (prodDeltaR - sigma * sigma);
+        if (d < 0) return null;
+
+        return -(prodCross + Math.sqrt(d)) / prodDeltaV;
+    }
+
+    /**
+     * Update particle to simulate it bouncing off a vertical wall
+     */
+    public void bounceX() {
+        this.vx = -this.vx;
+        this.collisionCount++;
+    }
+
+    /**
+     * Update particle to simulate it bouncing off a horizontal wall
+     */
+    public void bounceY() {
+        this.vy = -this.vy;
+        this.collisionCount++;
+    }
+
+    /**
+     * Update particle and other simulating them bouncing off together
+     * @param other particle that collides with self
+     */
+    public void bounce(Particle other) {
+        // Xi = self, Xj = other
+        double deltaX = other.x - this.x, deltaY = other.y - this.y;
+        double deltaVX = other.vx - this.vx, deltaVY = other.vy - this.vy;
+        double sigma = this.r + other.r;
+        double prodCross = deltaVX * deltaX + deltaVY * deltaY;
+
+        double J = (2 * this.m * other.m * prodCross) / (sigma * (this.m + other.m));
+        double Jx = J * deltaX / sigma, Jy = J * deltaY / sigma;
+
+        this.vx = this.vx + Jx / this.m;
+        this.vy = this.vy + Jy / this.m;
+        this.collisionCount++;
+
+        other.vx = other.vx - Jx / other.m;
+        other.vy = other.vy - Jy / other.m;
+        other.collisionCount++;
+    }
+
+    public int getCollisionCount() {
+        return collisionCount;
     }
 
     @Override
